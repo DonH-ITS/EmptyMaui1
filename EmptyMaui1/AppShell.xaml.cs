f8d42b6454c4df16a5770c88ec36869b9b5584ae
Donny <32511652+DonH-ITS@users.noreply.github.com>
2024-11-28 13:14:58 +0000

    Add project files.

---
+++ EmptyMaui1/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EmptyMaui1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EmptyMaui1/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace EmptyMaui1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
